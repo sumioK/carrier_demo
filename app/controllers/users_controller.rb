696930f329f13d5a8765ff543a6e637f077345e4
Sumio K <tap.miwo@gmail.com>
2022-08-20 19:48:33 +0900

    add avatar colums

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:nickname, :age)
+      params.require(:user).permit(:nickname, :age, :avatar)
     end
 end
